f40b9f09c947c36e422d2d453b176bdd46c9964b
kvora2 <kelvinvora21@gmail.com>
2023-01-13 12:52:29 -0500

    commited

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  BTI425 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Kelvin Vora Student ID: 157616210 Date: 13th Jan 2023
+*  Cyclic Link: 
+*  
+********************************************************************************/ 
+
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
@@ -6,53 +17,62 @@ require('dotenv').config()
 app.use(cors())
 const HTTP_PORT = process.env.PORT
 
-const { rmSync } = require('fs')
 const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
 
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send({ Message: "API Listening" })
+    res.status(201).send({ Message: "API Listening" })
 })
 
 app.post("/api/movies", (req, res) => {
     db.addNewMovie(req.body).then((movie) => {
-        res.status(201).json({NewMovieAdded: movie});
+        res.status(201).json({ NewMovieAdded: movie });
     }).catch((err) => {
-        res.status(500).json({Error: err})
+        res.status(500).json({ Error: err })
     })
 })
 
 app.get("/api/movies", (req, res) => {
     db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((movies) => {
-        res.json(movies)
+        if (movies == null) {
+            res.status(204).send("No Content found!")
+        }
+        else {
+            res.json(movies)
+        }
     }).catch((err) => {
-        res.status(500).json({Error: err})
+        res.status(500).json({ Error: err })
     })
 })
 
 app.get("/api/movies/:id", (req, res) => {
     db.getMovieById(req.params.id).then((movie) => {
-        res.send({MovieById: movie})
+        if (movie == null) {
+            res.status(204).json("No Content found!")
+        }
+        else {
+            res.send({ MovieById: movie })
+        }
     }).catch((err) => {
-        res.status(500).json({Error: err})
+        res.json(err)
     })
 })
 
 app.put("/api/movies/:id", (req, res) => {
     db.updateMovieById(req.body, req.params.id).then((movie) => {
-        res.send({UpdatedMovie: movie + " movie is been updated"})
+        res.send({ UpdatedMovie: movie + " movie is been updated" })
     }).catch((err) => {
-        res.status(500).json({Error: err})
+        res.status(500).json({ Error: err })
     })
 })
 
 app.delete("/api/movies/:id", (req, res) => {
     db.deleteMovieById(req.params.id).then((movie) => {
-        res.json({Msg: movie + "Movie deleted successfully"})
+        res.json({ Msg: movie + "Movie deleted successfully" })
     }).catch((err) => {
-        res.status(500).json({Error: err})
+        res.status(500).json({ Error: err })
     })
 })
 
@@ -61,5 +81,5 @@ db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
         console.log(`server listening on: ${HTTP_PORT}`);
     })
 }).catch((err) => {
-    res.status(500).json({Error: err})
+    res.status(500).json({ Error: err })
 });
